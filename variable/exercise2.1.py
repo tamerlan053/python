c631e5f4941173e7cc6a2d63640d7e0201d6396b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-11-18 12:41:51 +0100

    Delete variable directory

--- variable/exercise2.1.py
+++
@@ -1,7 +0,0 @@
-var1 = int(input("Input number 1: "))
-var2 = int(input("Input number 2: "))
-var3 = int(input("Input number 3: "))
-
-average = (var1 + var2 + var3) / 3
-
-print("The average is: ", average)
