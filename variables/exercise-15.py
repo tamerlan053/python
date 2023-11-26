4af0f192a0437ee759d781f3c02772e64e7803b4
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-11-26 16:11:58 +0100

    Create exercise-15.py

---
+++ variables/exercise-15.py
@@ -0,0 +1,7 @@
+number = int(input("Enter an integer: "))
+
+last_digit = number % 10
+last_two_digits = number % 100
+
+print(f"The last digit is: {last_digit}")
+print(f"The last two digits are: {last_two_digits}")
