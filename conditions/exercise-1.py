2001ac3cb684c92041508eb99194c64b6e4ffb76
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-11-28 23:47:55 +0100

    Create exercise-1.py

---
+++ conditions/exercise-1.py
@@ -0,0 +1,6 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+if number1 == number2:
+    print("The numbers are equal!")
+else:
+    print("The numbers are not equal!")
