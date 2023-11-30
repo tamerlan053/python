d73de34197be57865c40420b39afb9089955f650
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-11-30 23:11:26 +0100

    Update exercise-2.py

--- variables/exercise-2.py
+++ variables/exercise-2.py
@@ -1,3 +1,6 @@
+# The entrance fee for a zoo is 11 euros for adults and 6 euros for children under 12 years old.
+# Create a program that reads the number of adults and children from the keyboard and calculates and prints the total price.
+
 ADULT_PRICE = 11
 CHILD_PRICE = 6
 
