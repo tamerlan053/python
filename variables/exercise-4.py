ac82fac3db2212d2fafb533afc09f93df6174cf2
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-11-30 23:12:52 +0100

    Update exercise-4.py

--- variables/exercise-4.py
+++ variables/exercise-4.py
@@ -1,3 +1,6 @@
+# You enter the temperature in degrees Celsius via the keyboard.
+# The computer calculates the temperature in degrees Fahrenheit, which is then displayed on the screen. 
+
 temp_celsius = float(input("Enter the temperature in degrees Celsius: "))
 
 formula_fahrenheit = (9 / 5) * temp_celsius + 32
