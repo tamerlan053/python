17724bda0ade8f259b3e948604e1506fa435769d
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-11-30 23:09:53 +0100

    Update exercise-1.py

--- variables/exercise-1.py
+++ variables/exercise-1.py
@@ -1,3 +1,6 @@
+# Define three variables var1, var2, and var3. Prompt the user to input three integers for the three variables.
+# Calculate the average and store it in a variable average. Display the average.
+
 var1 = int(input("Input number 1: "))
 var2 = int(input("Input number 2: "))
 var3 = int(input("Input number 3: "))
