deca9a3857b02d02fa6a0a09ec4d4cdfddc12e92
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-11-30 23:12:14 +0100

    Update exercise-3.py

--- variables/exercise-3.py
+++ variables/exercise-3.py
@@ -1,3 +1,6 @@
+# Enter your height (in meters) and weight (in kilograms) via the keyboard.
+# Calculate your BMI and print it.
+
 height = float(input("Enter your height: "))
 weight = int(input("Enter your weight: "))
 
