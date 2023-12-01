63b3c9deec9224e8ffa1b9f01f320f8d148aa36e
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-12-01 13:21:15 +0100

    Update exercise-5.py

--- variables/exercise-5.py
+++ variables/exercise-5.py
@@ -1,3 +1,6 @@
+# You enter the number of degrees Fahrenheit via the keyboard, and the computer calculates the number of degrees Celsius, rounded to one decimal place.
+# This value is then printed.
+
 temp_fahrenheit = float(input("Enter the temperature in degrees Fahrenheit: "))
 
 formula_celsius = (temp_fahrenheit - 32) * (5 / 9)
