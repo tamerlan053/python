f03d38103b26f7ca4c75a4cb9fdba9dd000af43e
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-12-01 13:22:34 +0100

    Update exercise-6.py

--- variables/exercise-6.py
+++ variables/exercise-6.py
@@ -1,3 +1,6 @@
+# Write code that calculates the area of a circle using variables radius and pi = 3.14159.
+# Display the result as follows: "The area of the circle with radius ... is ...".
+
 PI = 3.14159
 
 radius = float(input("Enter the radius of the circle: "))
