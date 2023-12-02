45c78b5be9f984fc0029c13709378c79fa90f85b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-12-02 21:02:41 +0100

    Update exercise-10.py

--- variables/exercise-10.py
+++ variables/exercise-10.py
@@ -1,4 +1,5 @@
-# swap the values of two variables without using an additional variable
+# Can you think of a way to swap the contents of two numeric variables without using a third auxiliary variable?
+
 
 a = int(input("Enter value for a: "))
 b = int(input("Enter value for b: "))
