60eebe06a4d328f422e4b7137c7141a11fa358d9
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-12-02 20:59:54 +0100

    Update exercise-8.py

--- variables/exercise-8.py
+++ variables/exercise-8.py
@@ -1,3 +1,14 @@
+# What does my car cost? Price-conscious individuals want to know how much their car truly costs. Successively, the following is entered:
+
+# number of kilometers traveled per year (traveled_km)
+# consumption in liters per 100 km (consumption)
+# price of 1 liter of fuel (price_per_liter)
+
+# The expected output is:
+# the total costs per year for the specified number of kilometers
+# the cost per kilometer driven.
+
+
 annual_distance_km = int(input("Enter the number of kilometers driven per year: "))
 fuel_consumption_per_100km = float(input("Enter fuel consumption in liters per 100 kilometers: "))
 price_of_1_liter = float(input("Enter the price of 1 liter of fuel: "))
