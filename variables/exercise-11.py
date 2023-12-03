d1034fe2a40c77f7b4bd6857108980e1230df759
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-12-03 11:50:28 +0100

    Update exercise-11.py

--- variables/exercise-11.py
+++ variables/exercise-11.py
@@ -1,3 +1,11 @@
+# A phone company charges its customers a fixed amount of 20 euros every two months (connection, rental, maintenance).
+# A phone call within Belgium costs 12 euro cents (regardless of the duration of the call), and an international call costs 50 euro cents per started minute.
+# These rates do not include VAT.
+
+# You enter the number of Belgian calls and the number of minutes spent on international calls in the past month via the keyboard.
+# The program calculates the total payment for the past month. The VAT rate is 21%.
+
+
 VAT_PERCENTAGE = 0.21
 
 belgian_calls = int(input("Enter the number of Belgian calls: "))
