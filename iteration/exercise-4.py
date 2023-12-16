68b53a46a270853953adc493822f73b9e798b7ad
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-12-16 16:08:15 +0100

    Update exercise-4.py

--- iteration/exercise-4.py
+++ iteration/exercise-4.py
@@ -1,3 +1,8 @@
+# Write a program to calculate a person's weight on the Moon, Jupiter, and Mars. 
+# The weight should vary between 50 kg and 120 kg in increments of 5 kg. 
+# The following constants are used in your program: percentage of weight relative to Earth - Moon: 16.5%, Jupiter: 253.7%, and Mars: 37.8%
+
+
 MOON = 0.165
 JUPITER = 2.537
 MARS = 0.378
