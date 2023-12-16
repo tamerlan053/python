30c1551c5649e276990f7b3f339582a2b3bb1642
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-12-16 16:04:09 +0100

    Update exercise-1.py

--- iteration/exercise-1.py
+++ iteration/exercise-1.py
@@ -1,3 +1,6 @@
+# Prompt the user to input the weight of an apple in grams.
+# Display a table of the weights of 1 to 100 apples.
+
 weight = int(input("Enter the weight of an apple: "))
 
 for i in range(1, 101):
