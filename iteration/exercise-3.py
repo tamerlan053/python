72a3243508a1f3dfd77704025332c554b9ecdd74
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-12-16 16:06:27 +0100

    Update exercise-3.py

--- iteration/exercise-3.py
+++ iteration/exercise-3.py
@@ -1,3 +1,7 @@
+# Read a sequence of integers and print their sum.
+# This sequence ends with 0. Also, print the number of negative numbers entered.
+
+
 sum_total = 0
 neg_count = 0
 number = int(input("Enter an integer: "))
