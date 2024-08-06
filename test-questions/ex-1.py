f7e16c94bf00e88210ca54ed59e8a8ea26226c05
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-06 23:07:36 +0200

    Update ex-1.py
    output: true

--- test-questions/ex-1.py
+++ test-questions/ex-1.py
@@ -1,3 +1,12 @@
 Problem
 
 # Write a function that checks if two lists are permutations of each other. For example, the lists [1, 2, 3] and [3, 2, 1] are permutations of each other, while [1, 2, 3] and [1, 2, 2] are not.
+def are_permutations(list1, list2):
+    if len(list1) != len(list2):
+        return False
+    return sorted(list1) == sorted(list2)
+
+list1 = [1, 2, 3]
+list2 = [3, 2, 1]
+
+print(are_permutations(list1, list2))  # Output: True
