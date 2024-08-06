57d2c2a274436f542e556a989febf0d8ee40e83d
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-06 23:08:00 +0200

    Update ex-1.py
    output false

--- test-questions/ex-1.py
+++ test-questions/ex-1.py
@@ -10,3 +10,8 @@ list1 = [1, 2, 3]
 list2 = [3, 2, 1]
 
 print(are_permutations(list1, list2))  # Output: True
+
+list3 = [1, 2, 3]
+list4 = [1, 2, 2]
+
+print(are_permutations(list3, list4))  # Output: False
