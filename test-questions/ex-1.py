acc6f847b51fed05f72bf6d569c23c9c5ea22898
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-06 23:06:50 +0200

    Update ex-1.py
    problem added

--- test-questions/ex-1.py
+++ test-questions/ex-1.py
@@ -1,1 +1,3 @@
+Problem
 
+# Write a function that checks if two lists are permutations of each other. For example, the lists [1, 2, 3] and [3, 2, 1] are permutations of each other, while [1, 2, 3] and [1, 2, 2] are not.
