fe20c2d8febb73e9d8d44087ba6b6e938f830542
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-07 17:55:56 +0200

    Update ex-2.py
    problem added

--- test-questions/ex-2.py
+++ test-questions/ex-2.py
@@ -1,1 +1,3 @@
+Problem
 
+# Write a function that checks if a given string is a palindrome. A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
