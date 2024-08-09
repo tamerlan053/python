0ce8051873744d290f46e826b5d838bd7429eb6a
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-09 19:44:11 +0200

    Update ex-3.py
    examples

--- test-questions/ex-3.py
+++ test-questions/ex-3.py
@@ -8,3 +8,8 @@ def is_power_of_three(n):
     while n % 3 == 0:
         n //= 3
     return n == 1
+
+# Examples
+print(is_power_of_three(27))  # Output: True
+print(is_power_of_three(9))   # Output: True
+print(is_power_of_three(10))  # Output: False
