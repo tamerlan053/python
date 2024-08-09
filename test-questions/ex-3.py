9651bba74c83b9298fedcfb035052c1d7b804223
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-09 19:43:45 +0200

    Update ex-3.py
    Function added

--- test-questions/ex-3.py
+++ test-questions/ex-3.py
@@ -1,3 +1,10 @@
 Problem
 
 # Write a function that determines if a given integer is a power of three.
+
+def is_power_of_three(n):
+    if n <= 0:
+        return False
+    while n % 3 == 0:
+        n //= 3
+    return n == 1
