d6393e95884e27a414627c269ee34f4380df7bf7
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-10 23:46:24 +0200

    Update ex-4.py
    Examples added

--- test-questions/ex-4.py
+++ test-questions/ex-4.py
@@ -4,3 +4,7 @@ Problem
 
 def has_unique_characters(s):
     return len(s) == len(set(s))
+
+# Examples
+print(has_unique_characters("abcdef"))  # Output: True
+print(has_unique_characters("hello"))   # Output: False
