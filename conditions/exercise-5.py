5c99259e43117cb9aca539de2a5a2b5947c9f407
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-11 13:14:57 +0200

    Update exercise-5.py
    task added

--- conditions/exercise-5.py
+++ conditions/exercise-5.py
@@ -1,3 +1,8 @@
+# Exercise 5
+
+# The unit price of a certain item is €11.5. The VAT percentage is 21%. Ask the customer how many items they want to order. A 10% discount is given on the total price including VAT if the amount is greater than €1,000. 
+# Display the amount that the customer has to pay.
+
 VAT_PERCENTAGE = 0.21
 ITEM_PRICE = 11.5
 
