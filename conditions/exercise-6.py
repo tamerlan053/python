fa6a924a6a5a727d96ec17382c24027e878f691b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-11 13:12:52 +0200

    Update exercise-6.py
    task added

--- conditions/exercise-6.py
+++ conditions/exercise-6.py
@@ -1,3 +1,10 @@
+# Exercise 6
+# Create a program to calculate the purchase price of a DVD. Input the year of the film and its rating (a number between 1-5) via the keyboard. The base price is always 5 euros. For a film that is less than 2 years old, an additional euro is added. For films with a rating of 4 or 5, you pay 2 euros more. Print the price of the film.
+
+# Extension:
+# + For films with a rating of 3 or 2, you pay 1 euro more.
+# + Make sure that a film never costs more than 7 euros.
+
 BASE_PRICE = 5
 
 year = int(input("Enter the release year of the film: "))
