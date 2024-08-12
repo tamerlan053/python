1ba5a723bbd1953455e88f94eb4dab27efbc3436
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-12 20:59:34 +0200

    Update exercise-2.py
    task added

--- conditions/exercise-2.py
+++ conditions/exercise-2.py
@@ -1,3 +1,12 @@
+# Exercise 2
+
+# Enter the gross salary of an employee via the keyboard. The task is to calculate and print the annual holiday allowance and the annual contribution. The holiday allowance is 5% of the gross salary. If this holiday allowance is at least 350 euros, the annual contribution is equal to 8% of 350 euros. If the holiday allowance is less than 350 euros, the annual contribution is 8% of the holiday allowance.
+# Print the following for this employee:
+
+# -gross salary
+# -holiday allowance
+# -annual contribution
+
 gross_salary = float(input("Enter the gross salary: "))
 
 holiday_allowance = 0.05 * gross_salary
