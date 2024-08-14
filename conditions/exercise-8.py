9401a77005c55e67b30720cd7eddcb6df58a863b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-14 23:43:43 +0200

    Update exercise-8.py
    task added

--- conditions/exercise-8.py
+++ conditions/exercise-8.py
@@ -1,1 +1,15 @@
+# Exercise 8
+# The price of a plane ticket is determined by several factors, namely:
 
+# + The length of the flight or the distance in km
+#    - <1000 km: short-distance flight: 25 euro cents/km
+#    - 1000-2999 km: medium-distance flight: 20 euro cents/km
+#    - >2999 km: long-distance flight: 12 euro cents/km
+# + The type of flight or class:
+#   - economy class
+#   - charter -20%
+#   - business trip +30%
+
+# Write a program that calculates the price of a plane ticket. 
+# The distance in km and the class (1 = economy class, 2 = charter, and 3 = business trip) must be entered. 
+# Print the final ticket price. This price must be expressed in euros.
