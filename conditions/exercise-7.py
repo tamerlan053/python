3cf465dda3786785d5c2fa34bd4639d22f7b8ff7
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-16 16:43:52 +0200

    Update exercise-7.py

--- conditions/exercise-7.py
+++ conditions/exercise-7.py
@@ -1,4 +1,5 @@
 # Exercise 7
+
 # The results of a student obtained in 3 different exams must be entered via the keyboard. These results are out of 20, and only whole numbers are given. Determine the percentage obtained by this student and print it along with the corresponding grade:
 
 # < 60% insufficient
