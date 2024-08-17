27c2f57f516a10b1a4dd1ef6a4ddfb2c3058fa16
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-17 23:39:33 +0200

    Update exercise-9.py
    task added

--- conditions/exercise-9.py
+++ conditions/exercise-9.py
@@ -1,1 +1,10 @@
+# Exercise 9
 
+# Via the keyboard, two integers \( a \) and \( b \) and an operation code are entered. The operation code offers the following options:
+#  -Code 1: Addition (\( a + b \))
+#  -Code 2: Subtraction (\( a - b \))
+#  -Code 3: Multiplication (\( a \times b \))
+#  -Code 4: Square of \( a \)
+#  -Code 5: Square of \( b \)
+
+# The goal is to perform the operation specified by the code on the entered numbers \( a \) and \( b \), and print the result along with the entered numbers. If a code is entered that does not correspond to one of the above operation codes, the message "Invalid code" should be printed.
