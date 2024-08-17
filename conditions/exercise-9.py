cf0e171c0f4d6e95492a2b2a793d33a08d4c5e6a
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-17 23:40:48 +0200

    Update exercise-9.py
    problem solved

--- conditions/exercise-9.py
+++ conditions/exercise-9.py
@@ -8,3 +8,22 @@
 #  -Code 5: Square of \( b \)
 
 # The goal is to perform the operation specified by the code on the entered numbers \( a \) and \( b \), and print the result along with the entered numbers. If a code is entered that does not correspond to one of the above operation codes, the message "Invalid code" should be printed.
+
+a = int(input("Enter integer a: "))
+b = int(input("Enter integer b: "))
+code = int(input("Enter code (1=addition; 2=subtraction; 3=multiplication; 4=square of a; 5=square of b): "))
+
+if code == 1:
+    result = a + b
+elif code == 2:
+    result = a - b
+elif code == 3:
+    result = a * b
+elif code == 4:
+    result = a * a
+elif code == 5:
+    result = b * b
+else:
+    result = "Invalid code"
+
+print("The 2 numbers are", a, "and", b, "and the result is", result)
