4bb37a039c05b7a6d3b9b4380338e1ec96b637ac
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-18 15:16:48 +0200

    Update exercise-10.py
    task added

--- conditions/exercise-10.py
+++ conditions/exercise-10.py
@@ -1,1 +1,11 @@
+# Exercise 10
 
+# Write a program that allows the input of the age and the year of joining of a member of a tennis club, and based on this information calculates and prints their contribution. The following rules are to be followed:
+
+# -Base amount: 100 euros
+# -Discount: 14.5 euros if the member is younger than 21 or older than 60
+# -Discount: 2.5 euros per year of membership
+
+# The minimum amount to be paid is 62.5 euros.
+
+# Use a constant with the value of the current year.
