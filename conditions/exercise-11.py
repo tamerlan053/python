03761c712ed8542ec20e10deb10c1dcbabbbdf9f
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-19 21:33:14 +0200

    Update exercise-11.py
    task added

--- conditions/exercise-11.py
+++ conditions/exercise-11.py
@@ -1,1 +1,25 @@
+# Exercise 11
 
+# Price Calculation for a Holiday
+
+# Input:
+# - Number of stars: 1 - 5
+# - Code: O (only breakfast), H (half-board), V (full-board), A (all-inclusive)
+# - Number of nights
+# - Season: H (high season), L (low season), T (mid season)
+
+# The price for accommodation is calculated as follows:
+# - For a 1-star hotel, you pay 30 euros per night
+# - For a 2-3 star hotel, you pay 40 euros per night
+# - For a 4-5 star hotel, you pay 55 euros per night
+
+# The price for meals is calculated as follows:
+# - For only breakfast, you pay 20% of the accommodation costs
+# - For half-board, you pay 50% of the accommodation costs
+# - For full-board, you pay 60% of the accommodation costs
+
+# NOTE: For All-inclusive, you always pay (regardless of the number of stars) 
+# an additional fixed price of 80 euros per night on top of the full-board price.
+
+# An extra discount of 10% on the total price is given during the low season for codes O and H.
+# Enter the necessary information and print the price of a holiday for one person.
