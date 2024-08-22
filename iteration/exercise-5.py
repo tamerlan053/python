472fe58497d1ba660c85f7bb74452c9d4ab642f9
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-22 13:30:03 +0300

    Update exercise-5.py

--- iteration/exercise-5.py
+++ iteration/exercise-5.py
@@ -1,8 +1,9 @@
+# Exercise 5
+
 # Write a program that requests a number between 1 and 100 (exclusive). Ensure that any incorrect input is rejected.
 # Whenever an incorrect input occurs, display an error message on the screen. 
 # Either "Error! The number must be greater than 1" or "Error! The number must be less than 100".
 # The program stops when a correct input is provided. The number is then displayed on the screen.
-
 
 number = int(input("Enter a number between 1 and 100 (exclusive): "))
 
