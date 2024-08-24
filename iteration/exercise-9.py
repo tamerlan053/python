36b92e880ae5d1c3c2f43153b5f6b92b63052c46
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-24 22:20:42 +0300

    Update exercise-9.py
    problem solved

--- iteration/exercise-9.py
+++ iteration/exercise-9.py
@@ -1,3 +1,8 @@
 # Exercise 9
 
 # Write a program that prints the multiplication tables for the numbers 2 to 5 (inclusive).
+
+for i in range(2, 6):
+    for j in range(1, 11):
+        print(i * j, end="\t")
+    print()
