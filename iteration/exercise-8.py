08c73430ac2e5f2ebe29e93c28b5313a38a60b41
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-24 22:04:39 +0300

    Update exercise-8.py

--- iteration/exercise-8.py
+++ iteration/exercise-8.py
@@ -1,3 +1,5 @@
+# Exercise 8
+
 # Cyclists are participating in an individual time trial covering a distance of 36 km.
 # The registration number and time in seconds of each cyclist are entered via the keyboard.
 # The input process stops when a negative number is entered for the registration number.
@@ -6,7 +8,6 @@
 # Questions:
 # Who is the fastest cyclist?
 # What percentage of cyclists take longer than 1 hour?
-
 
 sum = 0
 counter = 0
