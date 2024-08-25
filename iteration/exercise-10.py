38c09c7081add396588e14efddd4c56ec3b097bf
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-25 13:41:05 +0300

    Update exercise-10.py
    task added

--- iteration/exercise-10.py
+++ iteration/exercise-10.py
@@ -1,1 +1,5 @@
+# Exercise 10
 
+# Write a program that uses nested loops to produce the following screen output.
+# The size of the triangle is provided via input from the keyboard.
+# For your information: the height and width of the triangle are equal.
