bd1831e8a46b454ecac0424422e7b9427dad904e
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-26 10:02:28 +0300

    Update exercise-11.py
    task added

--- iteration/exercise-11.py
+++ iteration/exercise-11.py
@@ -1,1 +1,12 @@
+# Exercise 11
 
+# The following data about employees of a company are entered:
+# -Employee number
+# -Gender: 0 = female; 1 = male
+# -Age
+# -Gross salary
+
+# The input ends when the employee number 0 is entered. Ensure input validation for gender.
+
+# Print the number of male employees who are older than 34 years, or have a salary of 1800 euros or more.
+# Print the number of female employees who are younger than 25 years.
