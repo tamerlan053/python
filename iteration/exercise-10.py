3558432ca7a2c8ca108be6cb6f3ebd71e3ccbc6b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-26 09:59:29 +0300

    Update exercise-10.py
    problem solved

--- iteration/exercise-10.py
+++ iteration/exercise-10.py
@@ -3,3 +3,10 @@
 # Write a program that uses nested loops to produce the following screen output.
 # The size of the triangle is provided via input from the keyboard.
 # For your information: the height and width of the triangle are equal.
+
+height = int(input("Enter the height of the triangle "))
+
+for i in range(height, 0, -1):
+    for j in range(i):
+        print("@", end=" ")
+    print()
