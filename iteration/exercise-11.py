5a1d52b715bd71924247080314374ad1073bd578
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-26 10:03:06 +0300

    Update exercise-11.py
    problem solved

--- iteration/exercise-11.py
+++ iteration/exercise-11.py
@@ -10,3 +10,28 @@
 
 # Print the number of male employees who are older than 34 years, or have a salary of 1800 euros or more.
 # Print the number of female employees who are younger than 25 years.
+
+number = int(input("Enter your employee number: "))
+male_count = 0
+female_count = 0
+
+while number != 0:
+    gender = int(input("Enter your gender (0=female, 1=male): "))
+
+    while gender != 0 and gender != 1:
+        gender = int(input("Incorrect gender: enter again (0=female, 1=male): "))
+
+    age = int(input("Enter your age: "))
+    gross_salary = int(input("Enter your gross salary: "))
+
+    if (gender == 1):
+        if (age > 34 or gross_salary >= 1800):
+            male_count += 1
+    else:
+        if (age < 25):
+            female_count += 1
+
+    number = int(input("Enter your employee number: "))
+
+print("The number of men who meet the condition is", male_count)
+print("The number of women who meet the condition is", female_count)
