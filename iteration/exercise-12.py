0d2dbcaeaca0dcd6478833dbcc3939a7ffe1d323
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-28 15:13:56 +0300

    Update exercise-12.py
    task added

--- iteration/exercise-12.py
+++ iteration/exercise-12.py
@@ -1,1 +1,9 @@
+# Exercise 12
 
+# A company wants to pay its employees a bonus that is proportional to the number of years of service. 
+# Calculate the individual bonus and the total bonus. 
+# For each employee, the following must be entered via the keyboard: the last name (entering "/" or "*" means the end of the program), first name, and the number of years of service. 
+# Ensure input control for the number of years of service, it must be a number between 0 and 40 years. 
+# The bonus is calculated as follows: the number of years of service * 25 euros. 
+# If the number of years of service is less than 5, the person receives 0 euros as a bonus. Print the last name, first name, number of years of service, and bonus for each person. 
+# At the end of the program, print the total bonus to be paid and the highest bonus.
