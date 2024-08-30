c1648824a9d0f8dae660562aa98215afd652f4ee
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-30 23:39:20 +0300

    Create exercise-2.py
    task added

---
+++ functions/exercise-2.py
@@ -0,0 +1,5 @@
+# Exercise 2
+
+# Write a function that returns whether a given year is a leap year or not. 
+# A leap year is divisible by 4, but not divisible by 100. 
+# If the year is divisible by 400, it is again a leap year. Make sure the result is a boolean.
