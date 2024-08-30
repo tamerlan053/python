1e8e843dc41706c5a8dd305e8ff0151c6e75c161
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-30 23:32:53 +0300

    Update exercise-1.py
    problem solved

--- functions/exercise-1.py
+++ functions/exercise-1.py
@@ -4,3 +4,21 @@
 # At the beginning of the program, he enters the value of the euro against the US dollar, for example, 1 euro = 1.072 US dollars (1.072 is entered). 
 # Subsequently, amounts in euros are entered, after which the value in US dollars is displayed. 
 # The conversion takes place via a function. The program ends when 0 is entered for the amount in euros.
+
+def convert(rate, amount):
+    value = rate * amount
+    return value
+
+def main():
+    exchange_rate = float(input("Enter exchange rate: "))
+    amount = int(input("Enter the amount to convert in euros: "))
+
+    while amount != 0:
+        total = convert(exchange_rate, amount)
+
+        print("Amount in dollars is", total)
+
+        amount = int(input("Enter the amount to convert in euros: "))
+
+if __name__ == '__main__':
+    main()
