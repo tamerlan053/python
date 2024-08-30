dede371306404a3e3492e02f4289f916ee041263
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-30 23:31:49 +0300

    Update exercise-1.py
    task added

--- functions/exercise-1.py
+++ functions/exercise-1.py
@@ -1,1 +1,6 @@
+# Exercise 1
 
+# A bank clerk wants to quickly calculate the value of a certain amount in euros in US dollars. 
+# At the beginning of the program, he enters the value of the euro against the US dollar, for example, 1 euro = 1.072 US dollars (1.072 is entered). 
+# Subsequently, amounts in euros are entered, after which the value in US dollars is displayed. 
+# The conversion takes place via a function. The program ends when 0 is entered for the amount in euros.
