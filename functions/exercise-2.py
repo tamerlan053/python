ce4e876f35f79ccccf4690e52b588403203f6309
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-08-31 12:19:22 +0300

    Update exercise-2.py
    problem solved

--- functions/exercise-2.py
+++ functions/exercise-2.py
@@ -3,3 +3,14 @@
 # Write a function that returns whether a given year is a leap year or not. 
 # A leap year is divisible by 4, but not divisible by 100. 
 # If the year is divisible by 400, it is again a leap year. Make sure the result is a boolean.
+
+def is_leap_year(year):
+    return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
+
+def main():
+    year = int(input("Enter the year: "))
+    output = is_leap_year(year)
+    print("Is this a leap year?", output)
+
+if __name__ == '__main__':
+    main()
