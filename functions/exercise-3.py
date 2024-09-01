0bfb4ade142a64ace3fad432209fca417baba198
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-01 14:31:39 +0200

    Update exercise-3.py
    task added

--- functions/exercise-3.py
+++ functions/exercise-3.py
@@ -1,1 +1,14 @@
+# Exercise 3
 
+# Write a program to calculate the membership fee for a tennis club. For each member, the following information is entered via the keyboard: name, age, number of children, income, and year of joining. Input ends when X or x is entered for the name.
+
+# For each member, the name and membership fee should be printed.
+# The membership fee is calculated as follows:
+# -normal starting fee €100 per year;
+# -members older than 60 years: a reduction of €15;
+# -members with children a reduction of €7.5 per child with a maximum of €35;
+# -members who have been a member for more than 20 years a reduction of €12.5;
+# -members with an income less than €7500 per year a reduction of €25.
+
+# The reductions can be accumulated but the minimum membership fee is €50.
+# The calculation of the membership fee takes place in a function.
