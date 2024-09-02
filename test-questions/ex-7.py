9e335e2cd5f9647ea91e6ccca57301cdc8a00999
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-02 22:59:07 +0200

    Update ex-7.py
    task added

--- test-questions/ex-7.py
+++ test-questions/ex-7.py
@@ -1,1 +1,5 @@
+Problem
 
+# Write a program to calculate the final price for hotel bookings. 
+# For each booking, the following information is entered via the keyboard: customer name, number of nights, room type (single, double, suite), and whether the customer is a member of the hotel's loyalty program.
+# Input ends when X or x is entered for the customer name.
