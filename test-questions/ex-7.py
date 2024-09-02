46a6a997c83af5473ab57f8e400e3b513e73940c
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-02 22:59:59 +0200

    Update ex-7.py
    additional information added

--- test-questions/ex-7.py
+++ test-questions/ex-7.py
@@ -3,3 +3,14 @@ Problem
 # Write a program to calculate the final price for hotel bookings. 
 # For each booking, the following information is entered via the keyboard: customer name, number of nights, room type (single, double, suite), and whether the customer is a member of the hotel's loyalty program.
 # Input ends when X or x is entered for the customer name.
+
+# For each customer, the name and final price should be printed.
+# The price is calculated as follows:
+# -Base price per night: 
+#   Single: €50, Double: €90, Suite: €150;
+# -A discount of 10% for loyalty program members;
+# -If the booking is for more than 5 nights, a discount of 5%;
+# -A maximum total discount of 20% is allowed;
+# -The final price must not be lower than €100, regardless of the discounts.
+
+# The calculation of the final price takes place in a function.
