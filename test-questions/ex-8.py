451511192604fac2595e0c9e8cce534751273fcc
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-03 22:50:08 +0200

    Update ex-8.py
    problem solved

--- test-questions/ex-8.py
+++ test-questions/ex-8.py
@@ -2,3 +2,16 @@ Problem
 
 # Write a function that determines if a given integer is a factorial number.
 # A factorial number is any number that can be written as n! = 1 * 2 * ... * n, where n is a non-negative integer.
+
+def is_factorial(n):
+    if n < 1:
+        return False
+    
+    factorial = 1
+    i = 1
+    
+    while factorial < n:
+        i += 1
+        factorial *= i
+    
+    return factorial == n
