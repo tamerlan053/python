72e7317c5f67eceb332bbd97efa6651f60bcd1f4
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-05 20:52:32 +0200

    Update exercise-4.py
    generate_random & give_feedback funcions added

--- functions/exercise-4.py
+++ functions/exercise-4.py
@@ -4,3 +4,16 @@
 # The computer provides feedback: "higher," "lower," or "congratulations, number guessed." 
 # Use a function to provide the feedback. The player can only guess 4 times! 
 # The game stops if the number is guessed or if the 4 attempts are over.
+
+import random
+
+def generate_random():
+    return random.randint(1, 10)
+
+def give_feedback(input_number, random_number):
+    if input_number < random_number:
+        return "higher"
+    elif input_number > random_number:
+        return "lower"
+    else:
+        return "congratulations, the number has been guessed"
