2b0e3dbf7091725240ef91ac4a6bb7c91604620d
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-05 20:51:41 +0200

    Update exercise-4.py
    task added

--- functions/exercise-4.py
+++ functions/exercise-4.py
@@ -1,1 +1,6 @@
+# Exercise 4
 
+# A random integer is generated between 1 and 10 (inclusive), and the user is allowed to try to guess it.
+# The computer provides feedback: "higher," "lower," or "congratulations, number guessed." 
+# Use a function to provide the feedback. The player can only guess 4 times! 
+# The game stops if the number is guessed or if the 4 attempts are over.
