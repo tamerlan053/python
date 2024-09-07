00c45fe3f6e8ca180c67f85af5ffd2d1aa3d0e3a
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-07 23:46:59 +0200

    Update ex-9.py
    requirements added

--- test-questions/ex-9.py
+++ test-questions/ex-9.py
@@ -3,3 +3,8 @@
 # Write a Python program that reads a paragraph of text from the user and counts the frequency of each word in the text. 
 # The program should then display the words in order of frequency, from most common to least common. 
 # If two words have the same frequency, display them in alphabetical order.
+
+### Requirements:
+# -Ignore case when counting words (i.e., "The" and "the" should be treated as the same word).
+# -Ignore punctuation (e.g., "hello!" and "hello" should be treated as the same word).
+# -Words with the same frequency should be sorted alphabetically.
