1714731c42854f19a73123370c21af3906bf7f45
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-07 23:46:01 +0200

    Update ex-9.py
    task added

--- test-questions/ex-9.py
+++ test-questions/ex-9.py
@@ -1,1 +1,5 @@
+# Exercise 9
 
+# Write a Python program that reads a paragraph of text from the user and counts the frequency of each word in the text. 
+# The program should then display the words in order of frequency, from most common to least common. 
+# If two words have the same frequency, display them in alphabetical order.
