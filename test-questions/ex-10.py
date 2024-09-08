f3520c078842dc4c2465c82c05e70700743919ff
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-08 23:08:53 +0200

    Update ex-10.py
    task added

--- test-questions/ex-10.py
+++ test-questions/ex-10.py
@@ -1,1 +1,5 @@
+# Exercise 10
 
+# Write a Python program that calculates and displays the first n numbers in the Fibonacci sequence. 
+# The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1. 
+# Your task is to prompt the user for the number of terms they wish to calculate and then display those terms.
