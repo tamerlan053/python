e939bc6457c987e99b9b592cd351915c471433f2
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-09 23:51:10 +0200

    Update ex-11.py
    task added

--- test-questions/ex-11.py
+++ test-questions/ex-11.py
@@ -1,1 +1,5 @@
+# Exercise 11
 
+# Write a Python program to count the number of vowels in a given string. 
+# The string should be input by the user. 
+# The program should display the total count of vowels (both uppercase and lowercase) and the count of each individual vowel.
