c5cc414677529e391c7a97be5dbe3b1286d118bd
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-09 23:49:16 +0200

    Update ex-10.py
    fibonacci function finished

--- test-questions/ex-10.py
+++ test-questions/ex-10.py
@@ -3,3 +3,11 @@
 # Write a Python program that calculates and displays the first n numbers in the Fibonacci sequence. 
 # The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1. 
 # Your task is to prompt the user for the number of terms they wish to calculate and then display those terms.
+
+def fibonacci(n):
+    sequence = []
+    a, b = 0, 1
+    while len(sequence) < n:
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
