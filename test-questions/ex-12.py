415103ff79d3327cbfd5cac798876fab22a6c484
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-12 17:47:47 +0200

    Update ex-12.py
    check for palindromes of length 3 or more

--- test-questions/ex-12.py
+++ test-questions/ex-12.py
@@ -15,3 +15,12 @@ def longest_palindromic_substring(s):
 
     for i in range(n):
         dp[i][i] = True
+
+    for i in range(n - 1):
+        if s[i] == s[i + 1]:
+            dp[i][i + 1] = True
+            start = i
+            max_len = 2
+            
+    for length in range(3, n + 1):  
+        for i in range(n - length + 1):
