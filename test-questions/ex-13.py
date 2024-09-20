1f0c9f35941f6e40761fcebc1699e36efea2332b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-20 23:53:25 +0200

    Update ex-13.py

--- test-questions/ex-13.py
+++ test-questions/ex-13.py
@@ -1,1 +1,5 @@
+Problem
 
+# Given a list of strings, write a Python function to group anagrams together. An anagram is a word or phrase formed by rearranging the letters of another word or phrase, typically using all the original letters exactly once.
+
+# For example, if the input list is ["eat", "tea", "tan", "ate", "nat", "bat"], the function should return:
