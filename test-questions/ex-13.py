ebb0fcacbd8bb3eec92083ad203f0149249cf4ad
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-21 23:18:25 +0200

    Update ex-13.py
    group_anagrams added

--- test-questions/ex-13.py
+++ test-questions/ex-13.py
@@ -3,3 +3,14 @@ Problem
 # Given a list of strings, write a Python function to group anagrams together. An anagram is a word or phrase formed by rearranging the letters of another word or phrase, typically using all the original letters exactly once.
 
 # For example, if the input list is ["eat", "tea", "tan", "ate", "nat", "bat"], the function should return:
+
+from collections import defaultdict
+
+def group_anagrams(words):
+    anagram_dict = defaultdict(list)
+    
+    for word in words:
+        sorted_word = ''.join(sorted(word))
+        anagram_dict[sorted_word].append(word)
+    
+    return list(anagram_dict.values())
