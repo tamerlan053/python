0a7c585d08facbc278337dcf204afca6fd40d57d
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-22 00:21:14 +0200

    Update ex-1.py

--- test-questions/ex-1.py
+++ test-questions/ex-1.py
@@ -1,6 +1,7 @@
 Problem
 
 # Write a function that checks if two lists are permutations of each other. For example, the lists [1, 2, 3] and [3, 2, 1] are permutations of each other, while [1, 2, 3] and [1, 2, 2] are not.
+
 def are_permutations(list1, list2):
     if len(list1) != len(list2):
         return False
