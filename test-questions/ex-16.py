63a584a3cb9cf1df2ab62e23ced51498de26212e
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-12-03 11:11:27 +0100

    Problem added to ex-16

--- test-questions/ex-16.py
+++ test-questions/ex-16.py
@@ -1,1 +1,9 @@
+Problem
 
+# Write a Python function to determine if two strings are isomorphic. 
+# Two strings are isomorphic if the characters in one string can be replaced to get the other string, with each character mapping uniquely.
+
+# For example:
+# The strings "egg" and "add" are isomorphic because e → a and g → d.
+# The strings "foo" and "bar" are not isomorphic because o → a and o → r would conflict.
+# The function should return True if the strings are isomorphic, and False otherwise.
