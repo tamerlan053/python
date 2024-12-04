10d52c54cbba6554f403fd41a6a9419bba0e901b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-12-04 20:08:06 +0100

    are_isomorphic function defined

--- test-questions/ex-16.py
+++ test-questions/ex-16.py
@@ -7,3 +7,10 @@ Problem
 # The strings "egg" and "add" are isomorphic because e → a and g → d.
 # The strings "foo" and "bar" are not isomorphic because o → a and o → r would conflict.
 # The function should return True if the strings are isomorphic, and False otherwise.
+
+def are_isomorphic(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    
+    mapping_s1_to_s2 = {}
+    mapping_s2_to_s1 = {}
