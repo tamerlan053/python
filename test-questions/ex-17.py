220818976f6fa54265afa6a272b441d01e56a01d
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-12-04 20:11:08 +0100

    Problem added to ex-17

--- test-questions/ex-17.py
+++ test-questions/ex-17.py
@@ -1,1 +1,12 @@
+Problem
 
+# Write a Python function to find the longest word in a list of strings that can be built one character at a time by other words in the list.
+
+# For example, if the input list is ["a", "banana", "app", "appl", "ap", "apply", "apple"], the function should return "apple".
+
+# Explanation:
+# "a" is a word.
+# "ap" is built from "a".
+# "app" is built from "ap".
+# "appl" is built from "app".
+# "apple" is built from "appl".
