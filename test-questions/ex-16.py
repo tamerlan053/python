41347107ab51c08bf76e0ca6a12d65b1647d1f23
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-12-04 20:08:45 +0100

    Update ex-16.py

--- test-questions/ex-16.py
+++ test-questions/ex-16.py
@@ -14,3 +14,16 @@ def are_isomorphic(s1, s2):
     
     mapping_s1_to_s2 = {}
     mapping_s2_to_s1 = {}
+    
+    for char1, char2 in zip(s1, s2):
+        if (char1 in mapping_s1_to_s2 and mapping_s1_to_s2[char1] != char2) or \
+           (char2 in mapping_s2_to_s1 and mapping_s2_to_s1[char2] != char1):
+            return False
+        mapping_s1_to_s2[char1] = char2
+        mapping_s2_to_s1[char2] = char1
+    
+    return True
+
+# Example usage:
+print(are_isomorphic("egg", "add"))  # Output: True
+print(are_isomorphic("foo", "bar"))  # Output: False
