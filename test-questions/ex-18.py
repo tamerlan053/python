ef7bee9cf43f6ee97ce65d8c539a42ddee91ee1d
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-12-06 23:58:22 +0100

    Update ex-18.py

--- test-questions/ex-18.py
+++ test-questions/ex-18.py
@@ -1,1 +1,3 @@
+Problem
 
+# Write a Python function to calculate the “majority element” in a list of integers. The majority element is the element that appears more than n // 2 times, where n is the size of the list. Assume the input list always has a majority element.
