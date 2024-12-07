6f9fd13749a04501b13adeca742e4071a1900627
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-12-07 18:57:52 +0100

    Update ex-19.py

--- test-questions/ex-19.py
+++ test-questions/ex-19.py
@@ -6,3 +6,19 @@ Problem
 # For the input "leetcode", the output should be "l".
 # For the input "loveleetcode", the output should be "v".
 # For the input "aabb", the output should be None.
+
+from collections import Counter
+
+def first_non_repeating_character(s):
+    char_count = Counter(s)
+    
+    for char in s:
+        if char_count[char] == 1:
+            return char
+    
+    return None
+
+# Example usage:
+s = "loveleetcode"
+result = first_non_repeating_character(s)
+print(result)  # Output: "v"
