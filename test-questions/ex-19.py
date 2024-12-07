9982a95cd61645301823e0d353eee7bd0754a7bd
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-12-07 18:57:09 +0100

    Ex-19 created

--- test-questions/ex-19.py
+++ test-questions/ex-19.py
@@ -1,1 +1,8 @@
+Problem
 
+# Write a Python function to find the first non-repeating character in a string. If all characters repeat, return None.
+
+# For example:
+# For the input "leetcode", the output should be "l".
+# For the input "loveleetcode", the output should be "v".
+# For the input "aabb", the output should be None.
